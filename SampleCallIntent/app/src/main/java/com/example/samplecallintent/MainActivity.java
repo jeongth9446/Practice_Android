11883c3dc9a7851c3eec3de1cfd763e0a740636e
jeongth9446 <52663419+jeongth9446@users.noreply.github.com>
2021-08-09 10:35:23 +0900

    Sample Call Intent

---
+++ SampleCallIntent/app/src/main/java/com/example/samplecallintent/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.samplecallintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText= (EditText) findViewById(R.id.editText);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String data = editText.getText().toString();
+
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(data));
+                startActivity(intent);
+            }
+        });
+    }
+}
