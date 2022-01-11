e00a74305336891cb686b3eb7dd6935d121c6415
jeongth9446 <52663419+jeongth9446@users.noreply.github.com>
2022-01-11 20:49:32 +0900

    머터리얼 디자인 사용 이제 메시지 디자인에 레이아웃이 생겼지만 아직 디자인은 그렇게 좋지는 않습니다.
    Jetpack Compose는 머티리얼 디자인 및 UI 요소를 즉시 사용 가능하도록 구현합니다. 머티리얼 디자인 스타일링을 사용하여 MessageCard 컴포저블의 디자인을 개선합니다.
    
    먼저 프로젝트(이 경우 ComposeTutorialTheme)에서 생성한 머티리얼 테마로 MessageCard 함수를 래핑합니다. @Preview 및 setContent 함수 모두에서 이 작업을 실행합니다.
    
    머티리얼 디자인은 색상, 서체, 도형의 세 가지 핵심 요소에 따라 이루어집니다. 하나씩 추가해 보겠습니다.

--- exMaterialDesign/app/src/main/java/com/example/exmaterialdesign/MainActivity.kt
+++ exMaterialDesign/app/src/main/java/com/example/exmaterialdesign/MainActivity.kt
@@ -1,38 +1,67 @@
 package com.example.exmaterialdesign
 
 import android.os.Bundle
+
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.exmaterialdesign.ui.theme.ExMaterialDesignTheme
+import androidx.compose.foundation.layout.Column
+
+data class Message(val author: String, val body: String)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ExMaterialDesignTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                MessageCard(Message("Android", "Jetpack Compose"))
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(msg: Message) {
+    // Add padding around our message
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.profile_picture),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+        )
+
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(text = msg.author)
+            // Add a vertical space between the author and message texts
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
+fun PreviewMessageCard() {
     ExMaterialDesignTheme {
-        Greeting("Android")
+        MessageCard(
+            msg = Message("Colleague", "Take a look at Jetpack Compose, it's great!")
+        )
     }
 }
