6aa61980d3490f8f8205baf8bedd600740418f68
jeongth9446 <52663419+jeongth9446@users.noreply.github.com>
2022-01-26 23:06:52 +0900

    Example How to Make a Clean Architecture Note App (2 / 2)

--- CleanArchitectureNoteApp/app/src/main/java/com/example/cleanarchitecturenoteapp/feature_note/domain/use_case/NoteUseCases.kt
+++ CleanArchitectureNoteApp/app/src/main/java/com/example/cleanarchitecturenoteapp/feature_note/domain/use_case/NoteUseCases.kt
@@ -3,6 +3,7 @@ package com.example.cleanarchitecturenoteapp.feature_note.domain.use_case
 data class NoteUseCases(
    val getNotes: GetNotes,
    val deleteNote: DeleteNote,
-   val addNote: AddNote
+   val addNote: AddNote,
+   val getNote: GetNote
 ) {
 }
